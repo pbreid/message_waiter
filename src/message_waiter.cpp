1fe227ecc4f971a196f066c7cc8fed69c37722f1
Peter Reid <pbreid@gmail.com>
2024-01-30 10:32:24 +1000

    changed from command line args to parameters

--- src/message_waiter.cpp
+++ src/message_waiter.cpp
@@ -3,36 +3,48 @@
 
 bool message_received = false;
 
-void chatterCallback(const topic_tools::ShapeShifter::ConstPtr& msg) {
+void chatterCallback(const topic_tools::ShapeShifter::ConstPtr &msg)
+{
     ROS_INFO("Message received on topic");
     message_received = true;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     ros::init(argc, argv, "message_waiter");
     ros::NodeHandle nh;
 
-    if (argc != 3) {
-        ROS_ERROR("Usage: message_waiter <topic> <command>");
+    std::string topic_to_listen;
+    std::string command_to_run;
+
+    if (!nh.getParam("topic", topic_to_listen))
+    {
+        ROS_ERROR("Failed to get param 'topic'");
         return -1;
     }
 
-    std::string topic_to_listen = argv[1];
-    std::string command_to_run = argv[2];
+    if (!nh.getParam("command", command_to_run))
+    {
+        ROS_ERROR("Failed to get param 'command'");
+        return -1;
+    }
 
     ros::Subscriber sub = nh.subscribe(topic_to_listen, 1, chatterCallback);
 
     ros::Rate loop_rate(1);
-    while (ros::ok() && !message_received) {
+    while (ros::ok() && !message_received)
+    {
         ROS_INFO_STREAM("Waiting for message on topic " << topic_to_listen << "...");
         ros::spinOnce();
         loop_rate.sleep();
     }
 
-    if (message_received) {
+    if (message_received)
+    {
         ROS_INFO("Message received, running command: %s", command_to_run.c_str());
         int result = system(command_to_run.c_str());
-        if (result != 0) {
+        if (result != 0)
+        {
             ROS_ERROR("Failed to execute command.");
         }
     }
